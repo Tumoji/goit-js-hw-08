43761c75579bf631135ad94cac8b7121afb2851f
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-11-04 00:35:15 +0200

    added javascript task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,21 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
-console.log(galleryItems);
+
+const galleryEl = document.querySelector(".gallery");
+
+galleryItems.forEach((image) => {
+  const li = document.createElement("li");
+  li.classList.add(".gallery__item");
+  li.innerHTML = ` <a class="gallery__link" href="${image.original}">
+    <img class ="gallery__image" src="${image.preview}" alt"=${image.description}" title="${image.description}"/>
+    </a>`;
+  galleryEl.append(li);
+});
+
+var lightbox = new SimpleLightbox(".gallery a", {
+  /* options */
+});
