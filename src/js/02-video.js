82847cc02e9137a357d66ed1d4d9f2988b028d3f
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-11-04 19:50:29 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,25 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+const savedTime = localStorage.getItem('videoplayer-current-time');
+
+let initialTime;
+
+if (savedTime) {
+  initialTime = parseFloat(savedTime);
+} else {
+  initialTime = 0;
+}
+
+player.setCurrentTime(initialTime);
+
+player.on(
+  'timeupdate',
+  throttle(function (data) {
+    const curentTime = data.seconds;
+    localStorage.setItem('videoplayer-current-time', curentTime.toFixed(2));
+  }, 1000)
+);
