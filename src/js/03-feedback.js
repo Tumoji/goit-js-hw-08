714d4f61a51887c809ae123308e0293bc2acf2e9
Tumoji <133051676+Tumoji@users.noreply.github.com>
2023-11-04 23:47:51 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const form = document.querySelector('.feedback-form');
+const emailInput = document.querySelector('input[name="email"]');
+const messageInput = document.querySelector('textarea[name="message"]');
+
+const saveForm = () => {
+  const submitForm = {
+    email: emailInput.value,
+    message: messageInput.value,
+  };
+  localStorage.setItem('feedback-form-state', JSON.stringify(submitForm));
+};
+
+const throttledSave = throttle(saveForm, 500);
+
+form.addEventListener('input', throttledSave);
+
+document.addEventListener('DOMContentLoaded', () => {
+  const savedForm = localStorage.getItem('feedback-form-state');
+  if (savedForm) {
+    const submitForm = JSON.parse(savedForm);
+    emailInput.value = submitForm.email;
+    messageInput.value = submitForm.message;
+  }
+});
+form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    const submitForm = {
+      email: emailInput.value,
+      message: messageInput.value,
+    };
+  
+    console.log('Form Data:', submitForm);
+  
+    localStorage.removeItem("feedback-form-state");
+
+    emailInput.value = '';
+    messageInput.value = '';
+  });
